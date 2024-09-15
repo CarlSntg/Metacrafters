15a245452946c93fdf3f9fe30142e153814c4b58
CarlSntg <106734911+CarlSntg@users.noreply.github.com>
2024-09-15 22:56:44 +0800

    Update Assessment.sol

--- 02-Solidity/Assessment.sol
+++ 02-Solidity/Assessment.sol
@@ -3,21 +3,21 @@ pragma solidity 0.8.26;
 
 contract MyToken {
 
-    // Public variables to store token details
+    // Public variables
     string public tokenName = "Carl";
     string public tokenAbbrv = "CRL";
     uint public totalSupply = 0;
 
-    // Mapping to store the balance of each address
+    // Mapping
     mapping(address => uint) public balances;
 
-    // Mint function to increase total supply and balance of an address
+    // Minting
     function mint(address _address, uint _value) public {
         totalSupply += _value;
         balances[_address] += _value;
     }
 
-    // Burn function to decrease total supply and balance of an address
+    // Burning
     function burn(address _address, uint _value) public {
         // Ensure the address has enough tokens to burn
         if (balances[_address] >= _value) {
