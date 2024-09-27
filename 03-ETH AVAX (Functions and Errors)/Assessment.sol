4e6391d986d03b2721cf03b53e26cf51d52c48df
CarlSntg <106734911+CarlSntg@users.noreply.github.com>
2024-09-28 01:27:11 +0800

    Update Assessment.sol

--- 03-ETH AVAX (Functions and Errors)/Assessment.sol
+++ 03-ETH AVAX (Functions and Errors)/Assessment.sol
@@ -1,32 +1,43 @@
-// SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract SmartContract {
-    uint256 public balance;
-    uint256 public constant MAX_WITHDRAWAL = 200;
+contract GymCheckInSystem {
+    uint8 public gymCapacity = 5;
+    uint8 public currentOccupancy = 0;
+    mapping(address => bool) public activeMembership;
+    mapping(address => bool) public isCheckedIn;
 
-    constructor() {
-        balance = 100;
+    // Function to activate membership
+    function activateMembership() external {
+        activeMembership[msg.sender] = true;
     }
 
-    // Modifier using assert() ensure the withdrawal doesn't exceed max withdrawal
-    modifier validWithdrawal(uint256 amount) {
-        assert(amount <= MAX_WITHDRAWAL);
-        _;
+    // Function to deactivate membership
+    function deactivateMembership() external {
+        activeMembership[msg.sender] = false;
+    }
+
+    // Function to check in to the gym
+    function checkIn() external {
+        require(activeMembership[msg.sender], "You must have an active membership to check in.");
         
+        require(currentOccupancy < gymCapacity, "Gym is at full capacity, please wait for someone to check out.");
+        
+        require(!isCheckedIn[msg.sender], "You are already checked in.");
+        
+        currentOccupancy++;
+        isCheckedIn[msg.sender] = true;
     }
 
-    // Function using require() to check deposit condition
-    function deposit(uint256 amount) public {
-        require(amount > 0, "Deposit amount must be greater than zero.");
-        balance += amount;
-    }
-
-    // Function using the validWithdrawal modifier and revert() to check withdrawal condition
-    function withdraw(uint256 amount) public validWithdrawal(amount) {
-        if (amount > balance) {
-            revert("Insufficient balance to withdraw.");
+    // Function to check out of the gym
+    function checkOut() external {
+        if (!isCheckedIn[msg.sender]) {
+            revert("You are not checked in.");
         }
-        balance -= amount;
+        
+        currentOccupancy--;
+        isCheckedIn[msg.sender] = false;
+
+        assert(currentOccupancy >= 0);
     }
 }
